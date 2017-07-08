255f2a9d54207891c1edeabbff4b5d9d149906b8
Daniel Nakushnov <daniel.nakushnov@gmail.com>
2017-07-09 01:42:51 +0300

    Placing player in the center of screen

--- engine.py
+++ engine.py
@@ -4,15 +4,18 @@ def main():
     screen_width = 80
     screen_height = 50
 
+    player_x = screen_width // 2
+    player_y = screen_height // 2
+
     tdl.set_font('arial10x10.png', greyscale=True, altLayout=True)
 
     root_console = tdl.init(screen_width, screen_height, title='Roguelike Tutorial Revised')
 
     while not tdl.event.is_window_closed():
-        root_console.draw_char(1, 1, '@', bg=None, fg=(255, 255, 255))
+        root_console.draw_char(player_x, player_y, '@', bg=None, fg=(255, 255, 255))
         tdl.flush()
 
-        root_console.draw_char(1, 1, ' ', bg=None)
+        root_console.draw_char(player_x, player_y, ' ', bg=None)
 
         for event in tdl.event.get():
             if event.type == 'KEYDOWN':
