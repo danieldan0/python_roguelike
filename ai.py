5f52260b003ac9d3ce75817779123ec0a2d866dc
Daniel Nakushnov <daniel.nakushnov@gmail.com>
2017-07-14 02:29:47 +0300

    Basic ECS prototype.

---
+++ ai.py
@@ -0,0 +1,4 @@
+class BasicMonster:
+    #AI for a basic monster.
+    def take_turn(self):
+        print('The ' + self.owner.name + ' growls!')
--- entity.py
+++ entity.py
@@ -1,8 +1,10 @@
+import math
+
 class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
-    def __init__(self, x, y, char, color, name, blocks=False):
+    def __init__(self, x, y, char, name, color, blocks = False, components, properties):
         self.x = x
         self.y = y
         self.char = char
@@ -10,10 +12,41 @@ class Entity:
         self.name = name
         self.blocks = blocks
 
+        #ECS stuff
+        self.components = []
+        for component in components:
+            self[component.name] = component
+            self[component.name].__init__(properties)
+            if self[component.name]:  #let the component know who owns it
+                self.[component.name].owner = self
+            self.components.append(component.name)
+
+    def has_component(component):
+        return (component.name in self.components)
+
     def move(self, dx, dy):
         # Move the entity by a given amount
         self.x += dx
         self.y += dy
+
+    def move_towards(self, target_x, target_y):
+        #vector from this object to the target, and distance
+        dx = target_x - self.x
+        dy = target_y - self.y
+        distance = math.sqrt(dx ** 2 + dy ** 2)
+
+        #normalize it to length 1 (preserving direction), then round it and
+        #convert to integer so the movement is restricted to the map grid
+        dx = int(round(dx / distance))
+        dy = int(round(dy / distance))
+        self.move(dx, dy)
+
+    def distance_to(self, other):
+        #return the distance to another object
+        dx = other.x - self.x
+        dy = other.y - self.y
+        return math.sqrt(dx ** 2 + dy ** 2)
+
 
 def get_blocking_entities_at_location(entities, destination_x, destination_y):
     for entity in entities:
